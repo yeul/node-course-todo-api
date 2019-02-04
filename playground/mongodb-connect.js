258ec5e1da6bd00867773bedf1445a5a991f8cff
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-04 16:06:49 -0500

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+// below using object restructuring to pull off more properties off the mongodb objet
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.')
+    }
+    console.log('Connected to MongoDB server')
+    const db = client.db('TodoApp')
+
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Finish Udemy Node.js course.',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert Todos', err)
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'Julia',
+        age: 25,
+        location: 'Maryland'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert Users', err)
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
+});
+
