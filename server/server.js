0c15a80a60623928c8846b367c46b02b80ce2c62
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-08 15:43:09 -0500

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
@@ -68,6 +69,32 @@ app.delete('/todos/:id', (req, res) => {
             res.status(400).send();
         });
     };
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    var valid = ObjectID.isValid(id);
+
+    if (!valid && id != null) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(PORT, () => {
